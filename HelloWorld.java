330ed5146555b1e11b06a6fc4d8be9551d403d9c
Jovan Radovic <jradovicmn@gmail.com>
2021-07-21 17:28:00 -0700

    BMI calculator

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,30 @@
+import java.util.*;
+
 public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please enter your weight in kgs.");
+
+    double weight = scanner.nextDouble();
+    
+    System.out.println("Please enter your height in centimeters");
+
+    double height = scanner.nextDouble()/100;
+
+    double BMI = (weight)/(height*height);
+
+    System.out.println("Your BMI is " + BMI + ".");
+
+    if (BMI > 25){
+        System.out.println("This indicates that you are overweight.");
+    } else if (BMI > 18.5) {
+        System.out.println("This indicates that you are healthy.");
+    } else {
+        System.out.println("This indicates that you are underweight.");
+    }
+
   }
 }
